4e70ae89a4cc785f07029ff94722c0e31c006046
kundankumarKD <40829686+kundankumarKD@users.noreply.github.com>
2025-06-01 16:14:02 +0530

    Update User.js

--- user-service/models/User.js
+++ user-service/models/User.js
@@ -1,4 +1,8 @@
 const { DataTypes } = require('sequelize');
+// const sequelize = new Sequelize('user_db', 'user', 'password', {
+//   host: 'db',
+//   dialect: 'mysql'
+// });
 const sequelize = require('../shared/db-config');
 
 const User = sequelize.define('User', {
