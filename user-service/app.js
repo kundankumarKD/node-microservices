757a9e4d24548c1fad07e06bd146eb63ca6babb5
kundankumarKD <40829686+kundankumarKD@users.noreply.github.com>
2025-06-01 15:43:02 +0530

    Update app.js

--- user-service/app.js
+++ user-service/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use('/users', userRoutes);
 
-sequelize.sync().then(() => {
+// sequelize.sync().then(() => {
   console.log('User DB synced');
   app.listen(3002, () => console.log('User service running on port 3002'));
-});
+// });
